ce37907b887669cc181064d5dcbc803f92f47567
evelynhelena <evelynhelena781@gmail.com>
2024-09-08 19:29:16 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import fastify from 'fastify'
 import { knex } from './database'
+// import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transactions = await knex('transactions')
+    .where('id', '0adb9482-0f0c-46a0-9dac-6628a707f9cd')
+    .select('*')
 
-  return tables
+  return transactions
 })
 
 app
